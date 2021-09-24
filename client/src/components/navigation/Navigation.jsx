122f86448da0ed25d9b7da24913b9bee44917aac
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-09-24 21:14:47 +0200

    Create Navigation and functionality

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./Navigation.scss";
 
+import { StoreContext } from "../../store/Store";
+
 const Navigation = () => {
+  const {
+    isOpenModalSignIn,
+    setIsOpenModalSignIn,
+    isOpenModalSignUp,
+    setIsOpenModalSignUp,
+  } = useContext(StoreContext);
+
+  const handleSignIn = () => {
+    if (!isOpenModalSignUp) setIsOpenModalSignIn(true);
+  };
+  const handleSignUp = () => {
+    if (!isOpenModalSignIn) setIsOpenModalSignUp(true);
+  };
+
   return (
     <header className="header">
       <nav className="header__nav">
         <ul className="header__nav-menu">
-          <li className="header__nav__link">Home</li>
-          <li className="header__nav__link">Sign in</li>
-          <li className="header__nav__link">Sign up</li>
+          <li className="header__nav-link">Home</li>
+          <li className="header__nav-link" onClick={handleSignIn}>
+            Sign in
+          </li>
+          <li className="header__nav-link" onClick={handleSignUp}>
+            Sign up
+          </li>
         </ul>
       </nav>
     </header>
