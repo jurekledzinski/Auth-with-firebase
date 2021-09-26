b720b1fe2a9af406a8d56b536cb406335a29e65e
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2021-09-26 09:58:13 +0200

    Display data from firestore

--- client/src/components/displayData/DisplayData.jsx
+++ client/src/components/displayData/DisplayData.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import "./DisplayData.scss";
 
-const data = [
-  { title: "Section 1", id: 0, text: "hello" },
-  { title: "Section 2", id: 1, text: "hello" },
-  { title: "Section 3", id: 2, text: "hello" },
-];
+import { StoreContext } from "../../store/Store";
 
 const DisplayData = () => {
+  const { dataBooks } = useContext(StoreContext);
   const [indexBtn, setIndexBtn] = useState(false);
 
   const handleSelectButton = (index) => {
@@ -19,29 +16,33 @@ const DisplayData = () => {
   return (
     <main className="main">
       <section className="display">
-        {data.map((item, index) => (
-          <div className="display__card" key={item.id}>
-            <button
-              className={
-                index === indexBtn
-                  ? "display__card-title display__card-title--active"
-                  : "display__card-title"
-              }
-              onClick={() => handleSelectButton(index)}
-            >
-              {item.title}
-            </button>
-            <div
-              className={
-                index === indexBtn
-                  ? "display__card-text display__card-text--active"
-                  : "display__card-text"
-              }
-            >
-              <p className="display__txt">{item.text}</p>
+        {dataBooks.length > 0 ? (
+          dataBooks.map((item, index) => (
+            <div className="display__card" key={item.id}>
+              <button
+                className={
+                  index === indexBtn
+                    ? "display__card-title display__card-title--active"
+                    : "display__card-title"
+                }
+                onClick={() => handleSelectButton(index)}
+              >
+                {item.title}
+              </button>
+              <div
+                className={
+                  index === indexBtn
+                    ? "display__card-text display__card-text--active"
+                    : "display__card-text"
+                }
+              >
+                <p className="display__txt">{item.author}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <h2 className="display__title">Log in to see content</h2>
+        )}
       </section>
     </main>
   );
